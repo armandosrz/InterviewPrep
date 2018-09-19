1fbc8b8ed1d6fff3a6b30ede5af1ee63592c1756
javier.suarezrivas@autozone.com <javier.suarezrivas@autozone.com>
2018-09-19 09:13:49 -0500

    Solved merge comflict

--- EPI/epi_judge_python/buy_and_sell_stock_twice.py
+++ EPI/epi_judge_python/buy_and_sell_stock_twice.py
@@ -1,7 +1,13 @@
 from test_framework import generic_test
 
+'''
+    Problem:
+    Write a program that tells you the max profit you can make by buying and selling a stock
+    twice. The second buy must be made on a date after the first sell.
+'''
 
 def buy_and_sell_stock_twice(prices):
+    print(prices)
     # TODO - you fill in here.
     return 0.0
 
