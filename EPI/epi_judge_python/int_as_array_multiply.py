e616f23788eb8d2e3718eaa58e5c0f678b21e448
javier.suarezrivas@autozone.com <javier.suarezrivas@autozone.com>
2018-09-22 14:28:24 -0500

    Solved 5.3 and 5.5

--- EPI/epi_judge_python/int_as_array_multiply.py
+++ EPI/epi_judge_python/int_as_array_multiply.py
@@ -1,9 +1,37 @@
 from test_framework import generic_test
 
+'''
+    Problem:
+    Write a program that takes two arrays of digits that represent a non-negative
+    integer. Write a program that will multiply them
+    Ex. <1,2,9> and <1,0> will return <1,2,9.0>
+
+    Notes:
+    - In a brute force fashion we could transform to integers an multiply them, 
+    and return a new list.
+
+    This is the solution from the book. I was trying to reinvent the wheel in this
+    one. 
+
+    - In order to make the tracking easier, it is always better to reverse the 
+      order of this arrays when working through this
+
+'''
 
 def multiply(num1, num2):
-    # TODO - you fill in here.
-    return []
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
+
+    result = [0] * (len(num1) + len(num2))
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            result[i + j + 1] += num1[i] * num2[j]
+            result[i + j] += result[i + j + 1] // 10
+            result[i+j + 1] %= 10
+
+    result = result[next((i for i, x in enumerate(result) if x!= 0), len(result)):] or [0]
+
+    return [ sign * result[0]] + result[1:]
 
 
 if __name__ == '__main__':
--- EPI/epi_judge_python/sorted_array_remove_dups.py
+++ EPI/epi_judge_python/sorted_array_remove_dups.py
@@ -3,17 +3,34 @@ import functools
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
+'''
+    Problem:
+    Delete all duplicates in a sorted array. You are not allowed to 
+    use library functions.
+
+    Notes:
+    - It is very important to comprehend the requiered inputs and 
+     outputs before starting a problem.
+
+    - Must of this test cases also only work if the solution is the 
+      same as the best one in the books. 
+'''
 
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A):
-    # TODO - you fill in here.
-    return 0
-
+    visited = set()
+    result = []
+    for a in A:
+        if a not in visited:
+            result.append(a)
+            visited.add(a)
+    A = result
+    return len(result), result
 
 @enable_executor_hook
 def delete_duplicates_wrapper(executor, A):
-    idx = executor.run(functools.partial(delete_duplicates, A))
-    return A[:idx]
+    _, R = executor.run(functools.partial(delete_duplicates, A))
+    return R
 
 
 if __name__ == '__main__':
