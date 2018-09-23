c8551d1a1d37deca181529b63f90a8e90941eab2
armando <armando>
2018-09-23 17:00:53 -0500

    Problem 5.17

--- EPI/epi_judge_python/is_valid_sudoku.py
+++ EPI/epi_judge_python/is_valid_sudoku.py
@@ -1,11 +1,45 @@
 from test_framework import generic_test
 
+'''
+    This particular problem focuses on implementing
+    clean code.
+'''
 
+
+import math
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment):
-    # TODO - you fill in here.
+    # check Horizontal
+    for row in partial_assignment:
+        if not all_unique(row):
+            return False
+    
+    # check vertical by doing a transpose 
+    for row in zip(*partial_assignment):
+        if not all_unique(row):
+            return False
+    
+    size = int(math.sqrt(len(partial_assignment)))
+
+    # Check all subsections
+    for a in range(size):
+        for b in range(size):
+            target = [
+                      partial_assignment[i][j]
+                      for i in range(a * size, size * (a+1))
+                      for j in range(b*size, size * (b+1))
+                     ]
+            if not all_unique(target):
+                return False
+
+
     return True
 
+
+def all_unique(A):
+    seen = set()
+    A = list(filter(lambda x: x!=0, A))
+    return not any(i in seen or seen.add(i) for i in A)
 
 if __name__ == '__main__':
     exit(
